81146c4a01d631f67ab0a38ac91b5928fcd514d3
Mike Wojtyna <mike@buildmysoftware.pro>
2019-11-23 21:52:06 +0100

    Fix typo in hello rabbit test

--- rabbitmq/src/test/java/pro/buildmysoftware/spring/rabbitmq/HelloRabbitIT.java
+++ rabbitmq/src/test/java/pro/buildmysoftware/spring/rabbitmq/HelloRabbitIT.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class HelloRabbitItIT {
+public class HelloRabbitIT {
 
 	@Autowired
 	private Producer producer;
